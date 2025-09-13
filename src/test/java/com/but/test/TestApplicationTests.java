e9aa58ddff8c159a1345900fa11689c2b97ac6c4
황정빈 <hjbin1211@gmail.com>
2025-09-13 18:30:05 +0900

    feat : 초기 파일 추가

---
+++ src/test/java/com/but/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.but.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
